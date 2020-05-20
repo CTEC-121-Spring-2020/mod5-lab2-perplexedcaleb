049308a6fb8713c0aba5165a5b26c081c25240f8
perplexedcaleb <c.howard14@students.clark.edu>
2020-05-20 12:10:54 -0700

    Completed code

---
+++ Mod5Lab2.py
@@ -0,0 +1,50 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+def dad():
+    print("Daddy finger, Daddy finger, where are you?")
+
+def mom():
+    print("Mommy finger, Mommy finger, where are you?")
+
+def brother():
+    print("Brother finger, Brother finger, where are you?")
+
+def sister():
+    print("Sister finger, Sister finger, where are you?")
+
+def baby():
+    print("Baby finger, Baby finger, where are you?")
+
+def here():
+    print("Here I am, here I am. How do you do?")
+
+def main():
+    print()
+    dad()
+    here()
+    print()
+    mom()
+    here()
+    print()
+    brother()
+    here()
+    print()
+    sister()
+    here()
+    print()
+    baby()
+    here()
+    print()
+    
+
+main()    
